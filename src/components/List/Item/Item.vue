217340f46f35bb68c157d55a7c00a45a8b35febb
Anthony Lu <58923223+tonylu110@users.noreply.github.com>
2022-06-22 13:00:01 +0800

    Add: set ok state

--- src/components/List/Item/Item.vue
+++ src/components/List/Item/Item.vue
@@ -5,13 +5,13 @@
         <span>{{ getTime(time as number) }}</span>
         <div>复制</div>
       </div>
-      <span class="item-text">
+      <span class="item-text" :style="okStyle">
         {{ text }}
       </span>
       <div class="close-button">
         <img src="/images/close.png" alt="" />
       </div>
-      <div class="ok-button">
+      <div class="ok-button" @click="setOk">
         <img src="/images/ok.png" alt="" />
       </div>
     </div>
@@ -19,12 +19,22 @@
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue';
 import getTime from '../../../util/getTime';
+import getOkStyle from '../../../data/getOkStyle'
+
+const okState = ref(false)
+const okStyle = ref({})
 
 const props = defineProps({
   time: Number,
   text: String,
 })
+
+const setOk = () => {
+  okState.value = !okState.value
+  okStyle.value = getOkStyle(okState.value)
+}
 </script>
 
 <style scoped lang="scss">
